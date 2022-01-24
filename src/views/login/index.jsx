0e19274f13e42d1db61d0e91b1787d2c6c019fe2
吴东 <511970747@qq.com>
2022-01-24 17:21:12 +0800

    init

---
+++ src/views/login/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Login() {
+  return (
+    <div className="Login">
+      <Link to="/">Order</Link>
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/views/order/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Order() {
+  return (
+    <div className="Order">
+      <Link to="/login">Login</Link>
+    </div>
+  );
+}
+
+export default Order;
